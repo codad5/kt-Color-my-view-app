8f4c60b8f445be7b527cd161fe6a5becfb8f0cce
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-02-24 13:17:18 +0100

    added color button

--- app/src/main/java/me/codad5/colormyview/MainActivity.kt
+++ app/src/main/java/me/codad5/colormyview/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListener()
     {
-        val clickableViews : List<View> = listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText, binding.boxFourText, binding.boxFiveText, binding.constraintLayout)
+        val clickableViews : List<View> = listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText, binding.boxFourText, binding.boxFiveText, binding.constraintLayout , binding.redButton, binding.yellowButton, binding.greenButton )
 
         for (item in clickableViews) {
             item.setOnClickListener {
@@ -38,6 +38,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> binding.boxFourText.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> binding.boxFiveText.setBackgroundResource(R.color.my_yellow)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
